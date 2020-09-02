6fb960dbd3fd764394ec2a7cc8cf5a45230d6484
Paulo Alexandre <furtuna.alexandre@gmail.com>
2020-09-02 11:57:05 -0500

    delete added to the API

--- index.js
+++ index.js
@@ -5,8 +5,8 @@ const port = 5000;
 
 let users = [
   {
-    id: "dkkjsu47",
-    name: "Jane Doe",
+    id: "lkjasdf84",
+    name: "Jane Doe", 
     bio: "Not Tarzan's Wife, another Jane",
   }
 ];
@@ -15,7 +15,42 @@ server.get(`/api/users`, (req, res) => {
   res.status(200).json(users);
 });
 
+server.delete(`/api/users/:id`, (req, res) => {
+  const { id } = req.params;
 
+  const userDeleted = users.find((user) => user.id === id);
+
+  if (userDeleted) {
+    users = users.filter((user) => user.id !== id);
+    res.status(200).json(userDeleted);
+  } else {
+    res.status(404).json({ message: "id not found" });
+  }
+});
+
+server.post(`/api/users`, (req, res) => {
+  const user = req.body;
+
+  user.id = shortid.generate();
+
+  users.push(user);
+  res.status(201).json(user);
+});
+
+server.get(`/api/users/:id`, (req, res) => {
+  const { id } = req.params;
+  const singleUser = req.body;
+
+  let foundUser = users.find((user) => user.id === id);
+
+  if (foundUser) {
+    Object.assign(foundUser, singleUser);
+    res.status(200).json(foundUser);
+  } else {
+    res.status(404).json({ message: "user not found!" });
+  }
+  res.status(201).json(res.params.id);
+});
 
 server.listen(port, () =>
   console.log(`Listening server on port ${port}, happy coding!`)
